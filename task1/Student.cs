df32fcae689ba5b64b29e7df8ed34e45df3a565e
kalenteryanv <kalenteryanv@mymacewan.ca>
2026-03-14 14:05:59 -0600

    Add comment to Student struct
    Added a comment explaining the purpose of the struct.

--- task1/Student.cs
+++ task1/Student.cs
@@ -1,5 +1,6 @@
-﻿namespace StudentAssignment
+namespace StudentAssignment
 {
+    // A struct is a value type used to hold small groups of related variables
     public struct Student
     {
         public int id;
